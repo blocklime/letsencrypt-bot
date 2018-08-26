10cdadfd8bda1431aa348e873edd2fe24d6cbf93
root <root@s148-66-134-158.secureserver.net>
2018-08-26 02:59:52 -0700

    It's working!!

--- letsencrypt-test.js
+++ letsencrypt-test.js
@@ -1,11 +1,15 @@
 #! /usr/bin/env node
 
 var ACME = '/home/wallet/public_html/.well-known/acme-challenge';
-var HOME = '.';
+var HOME = './ssl';
+var domains = [ 'wallet.nchain.ir' ];
+
+var G_LOCK = require('greenlock');
+var mkdirp = require('mkdirp');
 
 var store = require('le-store-certbot').create({
 		configDir: HOME,
-		privKeyPath: ':configDir/ssl.key/:hostname.key',
+		privkeyPath: ':configDir/ssl.key/:hostname.key',
 		fullchainPath: ':configDir/:hostname.full_ca_bundle',
 		certPath: ':configDir/ssl.crt/:hostname.crt',
 		chainPath: ':configDir/:hostname.ca_bundle',
@@ -20,12 +24,30 @@ var challenge = require('le-challenge-fs').create({
 		debug: false
 	});
 	
-var greenlock = require('greenlock').create({
+var greenlock = G_LOCK.create({
 		version: 'draft-12',
 		server: 'https://acme-v02.api.letsencrypt.org/directory',
 		store: store,
-		challenge: challenge
+		challenges: {
+			'http-01': challenge
+		},
+		challengeType: 'http-01'
+	});
+
+mkdirp.sync(HOME + '/ssl.key');
+mkdirp.sync(HOME + '/ssl.crt');
+greenlock.check({ domains: domains })
+	.then(ok => {
+		if (ok) return;
+		
+		greenlock.register({
+			domains: domains,
+			email: 'amouha@visionsoba.com',
+			agreeTos: true,
+			rsaKeySize: 2048,
+			challengeType: 'http-01'
+		})
+		.then(() => console.log('success'))
+		.catch(err => console.error(err));
 	});
 	
-console.log(greenlock.stagingServerUrl);
-console.log(greenlock.productionServerUrl);
